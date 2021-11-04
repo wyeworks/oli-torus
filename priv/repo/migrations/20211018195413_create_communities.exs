acd8a2ba3284c531a7e12ed4a273f844f489af6a
Gastón Abellá <gaston@wyeworks.com>
2021-11-04 13:31:55 -0300

    [MER-408] Add status column to communities schema

--- priv/repo/migrations/20211018195413_create_communities.exs
+++ priv/repo/migrations/20211018195413_create_communities.exs
@@ -3,14 +3,15 @@ defmodule Oli.Repo.Migrations.CreateCommunities do
 
   def change do
     create table(:communities) do
-      add :name, :string, null: false
-      add :description, :text
-      add :key_contact, :string
-      add :global_access, :boolean, default: true, null: false
+      add(:name, :string, null: false)
+      add(:description, :text)
+      add(:key_contact, :string)
+      add(:global_access, :boolean, default: true, null: false)
+      add(:status, :string, default: "active", null: false)
 
       timestamps(type: :timestamptz)
     end
 
-    create unique_index(:communities, [:name])
+    create(unique_index(:communities, [:name]))
   end
 end
