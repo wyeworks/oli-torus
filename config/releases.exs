7d59539165f61bcd760ced7471eef08ee6e01226
Mauricio Cassola <mauricio@wyeworks.com>
2022-03-28 17:25:23 -0300

    [MER-867] Force hsts header

--- config/releases.exs
+++ config/releases.exs
@@ -159,7 +159,8 @@ config :oli, OliWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT", "443"))
   ],
   secret_key_base: secret_key_base,
-  live_view: [signing_salt: live_view_salt]
+  live_view: [signing_salt: live_view_salt],
+  force_ssl: [rewrite_on: [:x_forwarded_proto], hsts: true]
 
 if System.get_env("SSL_CERT_PATH") && System.get_env("SSL_KEY_PATH") do
   config :oli, OliWeb.Endpoint,
